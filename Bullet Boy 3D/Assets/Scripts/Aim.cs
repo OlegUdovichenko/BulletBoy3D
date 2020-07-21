dba2ea3fefe33f12f342c96ffa1afcee777e1282
OlegUdovichenko <prostoolegoleg@gmail.com>
2020-07-21 14:59:42 +0300

    Quadratic aim with guidance. Cleaning after wringing out.

--- Bullet Boy 3D/Assets/Scripts/Aim.cs
+++ Bullet Boy 3D/Assets/Scripts/Aim.cs
@@ -5,61 +5,58 @@ using UnityEngine;
 public class Aim : MonoBehaviour
 {
     public LineRenderer lineRenderer;
-    public Transform point0, point1;
+    public Transform point0, point1, point2;
 
     private int quantity_points = 100;
     private Vector3[] _positions = new Vector3[100];
 
     private Vector3 touch_position;
     private float speed = 50;
-    private int aim_range = 30;
-    private Vector3 start_position_point;
 
     void Start()
     {
         lineRenderer.positionCount = quantity_points;
-        start_position_point = point1.position;
     }
 
-    // Update is called once per frame
     void FixedUpdate()
     {
         if(Input.GetMouseButton(0))
         {
             touch_position = Input.mousePosition;
-            touch_position.z = 10;
+            touch_position.z = 50;
             touch_position = Camera.main.ScreenToWorldPoint(touch_position);
 
-            touch_position.x = touch_position.x > 8 ? 8 : touch_position.x;
-            touch_position.x = touch_position.x < -8 ? -8 : touch_position.x;
-
-            //point1.position.z = aim_range;
+            touch_position.x = touch_position.x > 13 ? 13 : touch_position.x;
+            touch_position.x = touch_position.x < -13 ? -13 : touch_position.x;
             
-            point1.position = Vector3.MoveTowards(point1.position, new Vector3(touch_position.x, 
-                point1.position.y, aim_range), speed*Time.deltaTime);
+            point2.position = Vector3.MoveTowards(point2.position, new Vector3(touch_position.x, 
+                point2.position.y, point2.position.z), speed*Time.deltaTime);
 
-            DrawLinearCurve();
+            point1.position = Vector3.MoveTowards(point1.position, new Vector3(-touch_position.x, 
+                point1.position.y, point1.position.z), speed*Time.deltaTime);
+
+            DrawQuadraticCurve();
         }
-        if(Input.GetMouseButtonDown(0))
+        if(Input.GetMouseButtonUp(0))
         {
+            lineRenderer.positionCount = 0;
             Debug.Log("0");
-            point1.position = start_position_point;
         }
     }
 
-    private void DrawLinearCurve()
+    private void DrawQuadraticCurve()
     {
         float t;
         for(int i = 1; i < quantity_points + 1; i++)
         {
             t = i / (float)quantity_points;
-            _positions[i - 1] = CalculateLinearBezierPoint(t, point0.position, point1.position);
+            _positions[i - 1] = CalculateQuadraticBezierPoint(t, point0.position, point1.position, point2.position);
         }
         lineRenderer.SetPositions(_positions);
     }
 
-    private Vector3 CalculateLinearBezierPoint(float t, Vector3 p0, Vector3 p1)
+    private Vector3 CalculateQuadraticBezierPoint(float t, Vector3 p0, Vector3 p1, Vector3 p2)
     {
-        return p0 + t * (p1 - p0);
+        return ((1 - t) * (1 - t) * p0) + (2 * (1 - t) * t * p1) + (t * t * p2);
     }
 }
