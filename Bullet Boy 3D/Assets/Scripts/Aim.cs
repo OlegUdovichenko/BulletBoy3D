cc36a78603033e5249b5633da84e238e1634786c
OlegUdovichenko <prostoolegoleg@gmail.com>
2020-07-21 19:15:41 +0300

    Shot flying along the trajectory.

--- Bullet Boy 3D/Assets/Scripts/Aim.cs
+++ Bullet Boy 3D/Assets/Scripts/Aim.cs
@@ -6,12 +6,13 @@ public class Aim : MonoBehaviour
 {
     public LineRenderer lineRenderer;
     public Transform point0, point1, point2;
+    public BulletController bulletController;
 
     private int quantity_points = 100;
     private Vector3[] _positions = new Vector3[100];
 
     private Vector3 touch_position;
-    private float speed = 50;
+    private float _speed = 50;
 
     void Start()
     {
@@ -30,19 +31,22 @@ public class Aim : MonoBehaviour
             touch_position.x = touch_position.x < -13 ? -13 : touch_position.x;
             
             point2.position = Vector3.MoveTowards(point2.position, new Vector3(touch_position.x, 
-                point2.position.y, point2.position.z), speed*Time.deltaTime);
+                point2.position.y, point2.position.z), _speed*Time.deltaTime);
 
             point1.position = Vector3.MoveTowards(point1.position, new Vector3(-touch_position.x, 
-                point1.position.y, point1.position.z), speed*Time.deltaTime);
+                point1.position.y, point1.position.z), _speed*Time.deltaTime);
 
             DrawQuadraticCurve();
         }
+        
         if(Input.GetMouseButtonUp(0))
         {
             lineRenderer.positionCount = 0;
-            Debug.Log("0");
+            bulletController.Shot(point0.position, point1.position, point2.position);
         }
     }
+
+
 
     private void DrawQuadraticCurve()
     {
@@ -55,7 +59,7 @@ public class Aim : MonoBehaviour
         lineRenderer.SetPositions(_positions);
     }
 
-    private Vector3 CalculateQuadraticBezierPoint(float t, Vector3 p0, Vector3 p1, Vector3 p2)
+    public Vector3 CalculateQuadraticBezierPoint(float t, Vector3 p0, Vector3 p1, Vector3 p2)
     {
         return ((1 - t) * (1 - t) * p0) + (2 * (1 - t) * t * p1) + (t * t * p2);
     }
---
+++ Bullet Boy 3D/Assets/Scripts/BulletController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+    private float _speed = 30;
+    private bool _shot = false;
+    public Aim a;
+
+    private Vector3 p0, p1, p2;
+    private float t = 0;
+
+    void FixedUpdate()
+    {
+        if(_shot)
+        {
+            t+=0.01f;
+            transform.position = Vector3.MoveTowards(transform.position,
+                a.CalculateQuadraticBezierPoint(t, p0, p1, p2),
+                _speed*Time.deltaTime);
+
+            if(t >= 1)
+                _shot = false;
+        }
+    }
+    public void Shot(Vector3 p0, Vector3 p1, Vector3 p2)
+    {
+        _shot = true;
+        this.p0 = p0;
+        this.p1 = p1;
+        this.p2 = p2;
+    }
+}
