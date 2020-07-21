1ff9bac9034b1a80f28e17417e56e3415b24e916
OlegUdovichenko <prostoolegoleg@gmail.com>
2020-07-21 23:10:41 +0300

    Collection and storage of coins. Working with counters.

--- Bullet Boy 3D/Assets/Scripts/BulletController.cs
+++ Bullet Boy 3D/Assets/Scripts/BulletController.cs
@@ -4,40 +4,49 @@ using UnityEngine;
 
 public class BulletController : MonoBehaviour
 {
-    private float _speed = 30;
-    private bool _shot = false;
-    public Aim a;
+    public Aim aim;
+    public CountersController counters;
 
-    private Vector3 p0, p1, p2;
-    private float t = 0;
-    //private Rigidbody rb;
+    private float _speed = 10;
+    private bool _shot = false;
+
+    private Vector3 _p0, _p1, _p2;
+    private float _t = 0;
+    
 
     void FixedUpdate()
     {
         if(_shot)
         {
-            t+=0.01f;
+            _t+=0.01f;
             transform.position = Vector3.MoveTowards(transform.position,
-                a.CalculateQuadraticBezierPoint(t, p0, p1, p2),
+                aim.CalculateQuadraticBezierPoint(_t, _p0, _p1, _p2),
                 _speed * Time.deltaTime);
 
-            if(t >= 1)
+            if(_t >= 1)
                 _shot = false;
         }
     }
     public void Shot(Vector3 p0, Vector3 p1, Vector3 p2)
     {
         _shot = true;
-        this.p0 = p0;
-        this.p1 = p1;
-        this.p2 = p2;
+        _p0 = p0;
+        _p1 = p1;
+        _p2 = p2;
     }
 
     void OnTriggerEnter(Collider other)
     {
+        CoinController coin = other.GetComponent<CoinController>();
         TargetController target = other.GetComponent<TargetController>();
+        if(coin)
+        {
+            Progress.instance.coins++;
+            counters.RefreshCoinsBar();
+        }
         if(target)
         {
+            counters.RefreshObjective();
             Debug.Log("Vin");
         }
     }
---
+++ Bullet Boy 3D/Assets/Scripts/Progress.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Progress : MonoBehaviour
+{
+    public static Progress instance;
+
+    public int coins = 0;
+    void Awake()
+    {
+        if(instance == null)
+            instance = this;
+    }
+}
